b1f7ee17d28ff6748085370b926aa320ac404544
SpartakVardanyan <spartak.vardanyan.99@gmail.com>
2024-02-10 11:17:51 +0400

    fixx

--- src/Components/MainComponents/Comments/Comment.js
+++ src/Components/MainComponents/Comments/Comment.js
@@ -21,7 +21,7 @@ const Comment = ({ name, comment, userId }) => {
         <li className="comment">
             <div className="userIcon" style={{backgroundColor: comuserId ? "green" : "rgb(220, 89, 18)"}}><p>{firstLetter}</p></div>
             <div className="mainComment" style={{borderBottom: comuserId ? "1.5px solid green" : "1.5px solid rgb(220, 89, 18)"}}>
-                <h4>{comName || comuserId}</h4>
+                <h4 style={{color: comuserId ? "1.5px solid green" : "1.5px solid rgb(220, 89, 18)"}}>{comName || comuserId}</h4>
                 <p>{com}</p>
             </div>
         </li>
